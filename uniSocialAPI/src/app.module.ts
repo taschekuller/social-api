6b10588d6cfda536a8fe1fd8aaab7355be53e5a8
IDogukanINCE <71452988+taschekuller@users.noreply.github.com>
2023-09-04 10:29:23 +0300

    nest auth module generated

--- uniSocialAPI/src/app.module.ts
+++ uniSocialAPI/src/app.module.ts
@@ -4,12 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING), UsersModule],
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
